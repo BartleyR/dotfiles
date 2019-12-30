a282b2f2579aa7abbf47bbf232a1de3735425fee
Bartley Richardson <3215767+BartleyR@users.noreply.github.com>
2019-12-29 20:52:00 -0500

    Fixing user messages

--- bootstrap.ex.sh
+++ bootstrap.ex.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
 # Initialize variables
-$PATH_TO_PROJECTS = "$HOME/Projects"
+PATH_TO_PROJECTS="$HOME/Projects"
+LOC_HOME_FILES="home"
 
 # Create initial directories
 directories () {
-	echo "Checking to make sure the $PATH_TO_PROJECTS directory exists"
+	echo ""
+	echo " >> Checking to make sure the $PATH_TO_PROJECTS directory exists"
 
-	if [ ! -d "$PATH_TO_PROJECTS/Projects/" ] ; then
-		echo "Making directory $PATH_TO_PROJECTS"
+	if [ ! -d "$PATH_TO_PROJECTS" ] ; then
+		echo "    >> Making directory $PATH_TO_PROJECTS"
 		mkdir -p "$PATH_TO_PROJECTS"
 	else
-		echo "Directory already exists"
+		echo "    >> Directory already exists"
 	fi
 }
 
@@ -19,17 +21,18 @@ directories () {
 # Is this where rsync shines?
 # TODO - add support for -f and --force
 link_home() {
-	echo "Linking dotfiles from this repo to the home directory"
-	echo "Proceed? (y/n)"
+	echo ""
+	echo " >> Linking dotfiles from this repo to the home directory"
+	echo " >> Proceed? (y/n)"
 	read resp
 
 	if [ "$resp" = 'y' -o "$resp" = 'Y' ] ; then
-		for file in $( ls -A ./home/ | grep -vE '\.exclude*|\.git$|\.gitignore|.*.md' ) ; do
-			ln -sv "$PWD/home/$file" "$HOME"
+		for file in $( ls -A ./$LOC_HOME_FILES/ | grep -vE '\.exclude*|\.git$|\.gitignore|.*.md' ) ; do
+			ln -sv "$PWD/$LOC_HOME_FILES/$file" "$HOME"
 		done
-		echo "Symlinking to home directory complete"
+		echo "    >> Symlinking to home directory complete"
 	else
-		echo "Symlinking to home directory cancelled by user"
+		echo "    >> Symlinking to home directory cancelled by user"
 		return -1
 	fi
 }
