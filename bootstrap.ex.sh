d7d7f9781f8253ee1c0e90f3454ac42cda3c8797
Bartley Richardson <3215767+BartleyR@users.noreply.github.com>
2020-02-08 20:52:02 -0500

    Added Oh My ZSH install option Added Powerlevel9k theme install option Added Powerline font install instructions Updated README

--- bootstrap.ex.sh
+++ bootstrap.ex.sh
@@ -3,9 +3,22 @@ echo ""
 echo " >> Sourcing path variables"
 source ./config/.exports
 
+# Install Oh My ZSH
+oh_my_zsh() {
+	echo ""
+	sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	echo ""
+}
+
+# Install Powerlevel9k
+powerlevel9k() {
+	echo ""
+	git clone https://github.com/bhilburn/powerlevel9k.git $HOME/.oh-my-zsh/custom/themes/powerlevel9k
+	echo ""
+}
+
 # Create initial directories
 directories () {
-	echo ""
 	echo " >> Checking to make sure the $PATH_TO_PROJECTS directory exists"
 
 	if [ ! -d "$PATH_TO_PROJECTS" ] ; then
@@ -20,7 +33,6 @@ directories () {
 # Is this where rsync shines?
 # TODO - add support for -f and --force
 link_home() {
-	echo ""
 	echo " >> Linking dotfiles from this repo to the home directory"
 	read -p " >> Proceed? (y/n) : " resp
 
@@ -30,6 +42,7 @@ link_home() {
 			if [ -f $HOME/$file ] ; then
 				read -p "    >> $file already exists, overwrite? (y/n) : " resp
 				if [ "$resp" = 'y' -o "$resp" = 'Y' ] ; then
+					rm "$HOME/$file"
 					ln -sfv "$PWD/$LOC_HOME_FILES/$file" "$HOME"
 					echo ""
 				else
@@ -47,5 +60,60 @@ link_home() {
 	fi
 }
 
+# SUmmary post-install information
+summary() {
+	echo " >> Configuration complete"
+	echo " >> The script did not check to ensure Powerline fonts are installed."
+	echo " >> Visit https://github.com/powerline/fonts for install information."
+	echo ""
+	echo " >> If on a Linux system, you can run the command below to install Powerline fonts: "
+	echo ""
+	echo "     sudo apt-get install fonts-powerline"
+	echo ""
+}
+
+echo ""
+echo "Oh My ZSH Installation"
+echo "-----------------------------------------------------------------------------------"
+if [ ! -d "$HOME/.oh-my-zsh/" ] ; then
+	echo " >> Oh My ZSH is not installed at $HOME/.oh-my-zsh"
+	read -p " >> Would you like to install Oh My ZSH? (y/n) : " resp
+	if [ "$resp" = 'y' -o "$resp" = 'Y' ] ; then
+		oh_my_zsh
+	fi
+else
+	echo " >> Oh Oh My ZSH is installed at $HOME/.oh-my-zsh."
+	echo " >> Skipping install."
+fi
+
+echo ""
+echo "Powerlevel9k Theme"
+echo "-----------------------------------------------------------------------------------"
+if [ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel9k/" ] ; then
+	echo " >> The Powerlevel9k theme is not installed with Oh My ZSH. You may have it installed elsewhere (not checked)."
+	echo " >> The default configuration of this repo requires the Powerlevel9k theme."
+	read -p " >> Would you like to install the Powerlevel9k theme for Oh My ZSH? (y/n) : " resp
+	if [ "$resp" = 'y' -o "$resp" = 'Y' ] ; then
+		powerlevel9k
+	else
+		echo " >> Skipping the Powerlevel9k installation."
+	fi
+else
+	echo " >> The Powerlevel9k theme is already installed for Oh My ZSH."
+	echo " >> Skipping install."
+fi
+
+echo ""
+echo "Checking Directory Structure"
+echo "-----------------------------------------------------------------------------------"
 directories
+
+echo ""
+echo "Symbolinc Linking to Home Directory"
+echo "-----------------------------------------------------------------------------------"
 link_home
+
+echo ""
+echo "Post Bootstrap Summary"
+echo "-----------------------------------------------------------------------------------"
+summary
